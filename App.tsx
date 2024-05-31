e7616480b1cc6d939f1ca79e3a1256e81efe3947
weihanyau <weihanyau@gmail.com>
2024-05-31 22:12:54 +0800

    setup tamagui

--- App.tsx
+++ App.tsx
@@ -1,26 +1,41 @@
+/* eslint-disable global-require */
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { TamaguiProvider } from 'tamagui';
 import { Screens } from './app/navigation/Screens';
+import { appConfig } from './tamagui.config';
 
 SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient();
 
 // eslint-disable-next-line import/no-default-export
 export default function App() {
+    const [loaded] = useFonts({
+        Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+        InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     const onNavigationReady = () => {
         SplashScreen.hideAsync();
     };
 
     return (
-        <SafeAreaProvider>
-            <QueryClientProvider client={queryClient}>
-                <NavigationContainer onReady={onNavigationReady}>
-                    <Screens />
-                </NavigationContainer>
-            </QueryClientProvider>
-        </SafeAreaProvider>
+        <TamaguiProvider config={appConfig}>
+            <SafeAreaProvider>
+                <QueryClientProvider client={queryClient}>
+                    <NavigationContainer onReady={onNavigationReady}>
+                        <Screens />
+                    </NavigationContainer>
+                </QueryClientProvider>
+            </SafeAreaProvider>
+        </TamaguiProvider>
     );
 }
--- app/modules/onboarding/screens/OnboardingGenderSelectionScreen.tsx
+++ app/modules/onboarding/screens/OnboardingGenderSelectionScreen.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export function OnboardingGenderSelectionScreen() {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Onboarding Gender Selection Screen</Text>
-        </View>
+        </SafeAreaView>
     );
 }
