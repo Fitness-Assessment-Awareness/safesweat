5e48c7bbb20267f5a55b060b724827d53e5b1f5c
weihanyau <weihanyau@gmail.com>
2024-06-03 23:07:43 +0800

    add tab bar label

--- app/navigation/Screens.tsx
+++ app/navigation/Screens.tsx
@@ -11,59 +11,61 @@ function HomeTabScreens() {
     return (
         <Tab.Navigator
             initialRouteName="WorkoutStack"
-            screenOptions={({ route }) => ({
+            screenOptions={{
                 headerShown: false,
-                tabBarIcon: ({ size, color }) => {
-                    switch (route.name) {
-                        case 'WorkoutStack':
-                            return (
-                                <Dumbbell
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        case 'ExploreStack':
-                            return (
-                                <Globe2
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        case 'CatalogueStack':
-                            return (
-                                <BookMarked
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        case 'SettingsStack':
-                            return (
-                                <Settings
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        default:
-                            return null;
-                    }
-                },
-            })}
+            }}
         >
             <Tab.Screen
                 name="WorkoutStack"
                 component={WorkoutScreens}
+                options={{
+                    tabBarLabel: 'Workout',
+                    tabBarIcon: ({ size, color }) => (
+                        <Dumbbell
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="ExploreStack"
                 component={ExploreScreens}
+                options={{
+                    tabBarLabel: 'Explore',
+                    tabBarIcon: ({ size, color }) => (
+                        <Globe2
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="CatalogueStack"
                 component={CatalogueScreens}
+                options={{
+                    tabBarLabel: 'Catalogue',
+                    tabBarIcon: ({ size, color }) => (
+                        <BookMarked
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="SettingsStack"
                 component={SettingsScreens}
+                options={{
+                    tabBarLabel: 'Settings',
+                    tabBarIcon: ({ size, color }) => (
+                        <Settings
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
             />
         </Tab.Navigator>
     );
