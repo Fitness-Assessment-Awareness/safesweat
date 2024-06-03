7acd88df1c86c2465fea93e2647b434850433384
weihanyau <weihanyau@gmail.com>
2024-06-03 23:04:12 +0800

    add icons to tab bar

--- app/navigation/Screens.tsx
+++ app/navigation/Screens.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { BookMarked, Dumbbell, Globe2, Settings } from '@tamagui/lucide-icons';
 import { CatalogueScreens } from '../modules/catalogue/navigation/CatalogueScreenGroup';
 import { ExploreScreens } from '../modules/explore/navigation/ExploreScreenGroup';
 import { OnboardingScreenGroup } from '../modules/onboarding/navigation/OnboardingScreenGroup';
@@ -9,7 +11,43 @@ function HomeTabScreens() {
     return (
         <Tab.Navigator
             initialRouteName="WorkoutStack"
-            screenOptions={{ headerShown: false }}
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarIcon: ({ size, color }) => {
+                    switch (route.name) {
+                        case 'WorkoutStack':
+                            return (
+                                <Dumbbell
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        case 'ExploreStack':
+                            return (
+                                <Globe2
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        case 'CatalogueStack':
+                            return (
+                                <BookMarked
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        case 'SettingsStack':
+                            return (
+                                <Settings
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        default:
+                            return null;
+                    }
+                },
+            })}
         >
             <Tab.Screen
                 name="WorkoutStack"
