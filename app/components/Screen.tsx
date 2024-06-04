c161b6e37bceee035adc84501812c52c38e58917
weihanyau <weihanyau@gmail.com>
2024-06-05 00:47:28 +0800

    done weekly target

---
+++ app/components/Screen.tsx
@@ -0,0 +1,4 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styled } from 'tamagui';
+
+export const Screen = styled(SafeAreaView);
--- app/modules/workout/screens/WorkoutLandingScreen.tsx
+++ app/modules/workout/screens/WorkoutLandingScreen.tsx
@@ -1,12 +1,19 @@
+import { Pencil } from '@tamagui/lucide-icons';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { XStack, YStack } from 'tamagui';
+import { Card, Circle, XStack, YStack } from 'tamagui';
 import { Heading } from '../../../components/Heading';
+import { Label } from '../../../components/Label';
 import { Paragraph } from '../../../components/Paragraph';
+import { Screen } from '../../../components/Screen';
 
 export function WorkoutLandingScreen() {
+    const daysInWeek: number[] = [6, 7, 8, 9, 10, 11, 12];
     return (
-        <SafeAreaView>
+        <Screen
+            px="$3"
+            py="$8"
+            rowGap="$4"
+        >
             <XStack justifyContent="space-evenly">
                 <YStack
                     justifyContent="center"
@@ -30,6 +37,39 @@ export function WorkoutLandingScreen() {
                     <Paragraph>MINUTE</Paragraph>
                 </YStack>
             </XStack>
-        </SafeAreaView>
+            <Card
+                backgroundColor="#E7EBFB"
+                p="$4"
+                borderRadius="$8"
+                rowGap="$3"
+            >
+                <XStack justifyContent="space-between">
+                    <XStack
+                        alignItems="center"
+                        columnGap="$2"
+                    >
+                        <Label>Weekly Target</Label>
+                        <Pencil
+                            color="black"
+                            size="$1"
+                        />
+                    </XStack>
+                    <Label color="$green10">2/3</Label>
+                </XStack>
+                <XStack
+                    justifyContent="space-evenly"
+                    columnGap="$4"
+                >
+                    {daysInWeek.map((day) => (
+                        <Circle
+                            size="$4"
+                            backgroundColor="#DCE0EC"
+                        >
+                            <Heading size="small">{day}</Heading>
+                        </Circle>
+                    ))}
+                </XStack>
+            </Card>
+        </Screen>
     );
 }
