01bfe1500a5fcbac1f1c61872a2c6f8d889eff26
weihanyau <weihanyau@gmail.com>
2024-06-06 00:41:39 +0800

    standardise android header

--- app/modules/workout/screens/WorkoutPlanDetailsScreen.tsx
+++ app/modules/workout/screens/WorkoutPlanDetailsScreen.tsx
@@ -1,23 +1,30 @@
-import { Platform, StatusBar } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Image, ScrollView } from 'tamagui';
+import { StatusBar } from 'expo-status-bar';
+import { Image, ScrollView, View } from 'tamagui';
+import { Heading } from '../../../components/Heading';
 import { Screen } from '../../../components/Screen';
 import { WorkoutAssets } from '../assets';
 
 export function WorkoutPlanDetailsScreen() {
-    const insets = useSafeAreaInsets();
     return (
-        <Screen
-            flex={1}
-            pt={Platform.OS === 'android' && insets.top}
-        >
-            {Platform.OS === 'ios' ? <StatusBar barStyle="light-content" /> : null}
+        <Screen flex={1}>
+            <StatusBar style="light" />
             <ScrollView flex={1}>
-                <Image
-                    style={{ height: 250, width: '100%' }}
-                    objectFit="contain"
-                    source={WorkoutAssets.workoutBeginner}
-                />
+                <View>
+                    <Image
+                        style={{ height: 250, width: '100%' }}
+                        objectFit="contain"
+                        source={WorkoutAssets.workoutBeginner}
+                    />
+                    <Heading
+                        l="$4"
+                        b="$8"
+                        color="white"
+                        textShadowRadius={1}
+                        textShadowColor="black"
+                    >
+                        ABS BEGINNER
+                    </Heading>
+                </View>
             </ScrollView>
         </Screen>
     );
