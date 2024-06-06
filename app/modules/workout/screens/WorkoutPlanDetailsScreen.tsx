df52241026177e4efcc533e2b1d4a6ae192f853d
weihanyau <weihanyau@gmail.com>
2024-06-07 00:39:15 +0800

    Moved scroll view

--- app/modules/workout/screens/WorkoutPlanDetailsScreen.tsx
+++ app/modules/workout/screens/WorkoutPlanDetailsScreen.tsx
@@ -20,41 +20,41 @@ export function WorkoutPlanDetailsScreen() {
     return (
         <Screen flex={1}>
             <StatusBar style="light" />
-            <ScrollView flex={1}>
-                <View backgroundColor="red">
-                    <Image
-                        style={{ height: 250, width: '100%' }}
-                        objectFit="contain"
-                        source={absBeginner.thumbnail}
-                    />
-                    <Heading
-                        position="absolute"
-                        l="$4"
-                        b="$3"
-                        color="white"
-                        textShadowRadius={1}
-                        textShadowColor="black"
-                    >
-                        {absBeginner.title}
-                    </Heading>
-                </View>
-                <XStack
-                    py="$2"
-                    px="$4"
-                    justifyContent="space-between"
-                    alignItems="center"
+            <View backgroundColor="red">
+                <Image
+                    style={{ height: 250, width: '100%' }}
+                    objectFit="contain"
+                    source={absBeginner.thumbnail}
+                />
+                <Heading
+                    position="absolute"
+                    l="$4"
+                    b="$3"
+                    color="white"
+                    textShadowRadius={1}
+                    textShadowColor="black"
                 >
-                    <Paragraph>
-                        {absBeginner.estimatedDuration} MINS | {absBeginner.exercises.length} EXERCISES
-                    </Paragraph>
-                    <Button
-                        borderRadius="$8"
-                        themeInverse
-                    >
-                        History
-                    </Button>
-                </XStack>
-                <Separator borderColor="#D0D3D8" />
+                    {absBeginner.title}
+                </Heading>
+            </View>
+            <XStack
+                py="$2"
+                px="$4"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Paragraph>
+                    {absBeginner.estimatedDuration} MINS | {absBeginner.exercises.length} EXERCISES
+                </Paragraph>
+                <Button
+                    borderRadius="$8"
+                    themeInverse
+                >
+                    History
+                </Button>
+            </XStack>
+            <Separator borderColor="#D0D3D8" />
+            <ScrollView flex={1}>
                 <WorkoutExerciseOverview.Group
                     onValueChange={(value) => {
                         setOpen(true);
