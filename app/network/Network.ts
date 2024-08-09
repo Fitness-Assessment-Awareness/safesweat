9aefa3404d718bfdb58982b81b0e50bb5039268e
Wei Teng <weitengkoay@gmail.com>
2024-08-09 15:56:59 +0800

    feat: Setup axios network connection to backend

---
+++ app/network/Network.ts
@@ -0,0 +1,18 @@
+import { LOCAL_IP_ADDR } from '@env';
+import axios from 'axios';
+
+const getLocalIpAddr = () => LOCAL_IP_ADDR;
+
+export const Network = axios.create({
+    baseURL: `http://${getLocalIpAddr()}:8080`,
+    validateStatus: axios.defaults.validateStatus,
+    timeout: 12000,
+});
+
+Network.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error('An error occurred: ', error.message);
+        return Promise.reject(error);
+    },
+);
--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,13 @@ module.exports = function (api) {
         presets: ['babel-preset-expo'],
         plugins: [
             [
+                'module:react-native-dotenv',
+                {
+                    moduleName: '@env',
+                    path: '.env.local',
+                },
+            ],
+            [
                 '@tamagui/babel-plugin',
                 {
                     components: ['tamagui'],
