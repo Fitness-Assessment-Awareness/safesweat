a9f72dd5ec551f738cdce32e6f91a0e0df48841e
weihanyau <weihanyau@gmail.com>
2024-08-18 15:42:50 +0800

    fix environment variables

--- app/utils/Supabase.ts
+++ app/utils/Supabase.ts
@@ -1,11 +1,11 @@
-import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@env';
+import { EXPO_PUBLIC_SUPABASE_ANON_KEY, EXPO_PUBLIC_SUPABASE_URL } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { AppState } from 'react-native';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = SUPABASE_URL;
-const supabaseAnonKey = SUPABASE_ANON_KEY;
+const supabaseUrl = EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
