43130c1402d78802a790fbdbd06fc43274be3ea6
weihanyau <weihanyau@gmail.com>
2024-08-26 22:40:06 +0800

    done working out exercise info

--- app/modules/workout/screens/WorkoutStartInitialScreen.tsx
+++ app/modules/workout/screens/WorkoutStartInitialScreen.tsx
@@ -1,12 +1,17 @@
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
 import { RouteProp, useRoute } from '@react-navigation/native';
+import { Info } from '@tamagui/lucide-icons';
 import { StatusBar } from 'expo-status-bar';
 import LottieView from 'lottie-react-native';
+import { useRef, useState } from 'react';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { getTokenValue, View } from 'tamagui';
 import { Heading } from '../../../components/Heading';
 import { Label } from '../../../components/Label';
 import { Screen } from '../../../components/Screen';
+import { Sheet } from '../../../components/Sheet';
+import { WorkoutExerciseDetailsSheetContent } from '../components/WorkoutExerciseDetailsSheet';
 import { EXERCISES } from '../data/exercises';
 import { WORKOUTS } from '../data/workouts';
 import { WorkoutRootStackParamList } from '../navigation/WorkoutStackParamList';
@@ -15,51 +20,80 @@ export function WorkoutStartInitialScreen() {
     const { params } = useRoute<RouteProp<WorkoutRootStackParamList, 'WorkoutStartInitial'>>();
     const { workoutKey } = params;
     const insets = useSafeAreaInsets();
+    const sheetRef = useRef<BottomSheetModal>(null);
 
+    const [isCountingDown, setIsCountingDown] = useState(true);
     const workout = WORKOUTS[workoutKey];
     const firstWorkoutExercise = workout.exercises[0];
 
     return (
-        <Screen
-            flex={1}
-            pt={insets.top + getTokenValue('$8')}
-        >
-            <StatusBar
-                style="dark"
-                animated
-            />
-            <LottieView
-                source={EXERCISES[firstWorkoutExercise.exerciseKey].lottieSource}
-                autoPlay
-                loop
-                speed={1.5}
-                style={{ width: '100%', height: 300 }}
-            />
-            <View
+        <>
+            <Screen
                 flex={1}
-                justifyContent="center"
-                alignItems="center"
-                rowGap="$4"
+                pt={insets.top + getTokenValue('$8')}
             >
-                <Heading
-                    color="$blue11Light"
-                    size="large"
+                <StatusBar
+                    style="dark"
+                    animated
+                />
+                <LottieView
+                    source={EXERCISES[firstWorkoutExercise.exerciseKey].lottieSource}
+                    autoPlay
+                    loop
+                    speed={1.5}
+                    style={{ width: '100%', flex: 2 }}
+                />
+                <View
+                    flex={3}
+                    justifyContent="center"
+                    alignItems="center"
+                    rowGap="$4"
                 >
-                    READY TO GO!
-                </Heading>
-                <Label size="large">JUMPING JACKS</Label>
-                <View pt="$4">
-                    <CountdownCircleTimer
-                        isPlaying
-                        colors={getTokenValue('$blue11Light')}
-                        duration={20}
-                        size={100}
-                        strokeWidth={8}
+                    <Heading
+                        color="$blue11Light"
+                        size="large"
                     >
-                        {({ remainingTime }) => <Heading size="large">{remainingTime}</Heading>}
-                    </CountdownCircleTimer>
+                        READY TO GO!
+                    </Heading>
+                    <View
+                        flexDirection="row"
+                        alignItems="center"
+                        columnGap="$1"
+                    >
+                        <Label size="large">JUMPING JACKS</Label>
+                        <Info
+                            onPress={() => {
+                                sheetRef.current?.present();
+                                setIsCountingDown(false);
+                            }}
+                            hitSlop={4}
+                        />
+                    </View>
+                    <View pt="$4">
+                        <CountdownCircleTimer
+                            isPlaying={isCountingDown}
+                            colors={getTokenValue('$blue11Light')}
+                            duration={20}
+                            size={100}
+                            strokeWidth={8}
+                        >
+                            {({ remainingTime }) => <Heading size="large">{remainingTime}</Heading>}
+                        </CountdownCircleTimer>
+                    </View>
                 </View>
-            </View>
-        </Screen>
+            </Screen>
+            <Sheet
+                ref={sheetRef}
+                snapPoints={['85%']}
+                onDismiss={() => {
+                    setIsCountingDown(true);
+                }}
+            >
+                <WorkoutExerciseDetailsSheetContent
+                    {...EXERCISES[firstWorkoutExercise.exerciseKey]}
+                    {...firstWorkoutExercise}
+                />
+            </Sheet>
+        </>
     );
 }
