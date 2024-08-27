a2100594d3dfa1b936c1b3a9abc10ba16ec42b9c
weihanyau <weihanyau@gmail.com>
2024-08-28 01:52:29 +0800

    fix landing safe area

--- app/modules/onboarding/screens/OnboardingLandingScreen.tsx
+++ app/modules/onboarding/screens/OnboardingLandingScreen.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button, Progress, ScrollView, YStack } from 'tamagui';
 import { HeaderBackButton } from '../../../components/HeaderBackButton';
 import { useRootNavigation } from '../../../navigation/useAppNavigation';
@@ -70,75 +69,73 @@ export function OnboardingLandingScreen() {
     };
 
     return (
-        <SafeAreaView>
-            <ScrollView>
-                <YStack
-                    px="$4"
-                    justifyContent="space-between"
-                >
-                    <YStack>
-                        <HeaderBackButton
-                            canGoBack={progress > ONBOARDING_PROGRESS_STEPS.GENDER_SELECT}
-                            onPress={onPressBack}
-                        />
-                        <Progress
-                            size="$1"
-                            value={progress}
-                            alignSelf="center"
-                            width="30%"
-                        >
-                            <Progress.Indicator
-                                backgroundColor="darkblue"
-                                animation="bouncy"
-                            />
-                        </Progress>
-                        {progress === ONBOARDING_PROGRESS_STEPS.GENDER_SELECT && (
-                            <OnboardingGenderSelect
-                                gender={gender}
-                                setGender={setGender}
-                            />
-                        )}
-                        {progress === ONBOARDING_PROGRESS_STEPS.FOCUS_AREA_SELECT && (
-                            <OnboardingFocusAreaSelect
-                                focusAreas={focusAreas}
-                                setFocusAreas={setFocusAreas}
-                            />
-                        )}
-                        {progress === ONBOARDING_PROGRESS_STEPS.DIFFICULTY_SELECT && (
-                            <OnboardingDifficultySelect
-                                difficulty={difficulty}
-                                setDifficulty={setDifficulty}
-                            />
-                        )}
-                        {progress === ONBOARDING_PROGRESS_STEPS.HEALTH_PROBLEM_SELECT && (
-                            <OnboardingHealthProbSelect
-                                healthProblems={healthProblems}
-                                setHealthProblems={setHealthProblems}
-                            />
-                        )}
-                        {progress === ONBOARDING_PROGRESS_STEPS.BODY_INFO_SELECT && (
-                            <OnboardingBodyInfoSelect
-                                height={height}
-                                weight={weight}
-                                setHeight={setHeight}
-                                setWeight={setWeight}
-                            />
-                        )}
-                    </YStack>
-                    <Button
-                        backgroundColor="darkblue"
-                        pressStyle={{ backgroundColor: '$blue11' }}
-                        disabledStyle={{ backgroundColor: '$blue7' }}
-                        color="white"
-                        m="$4"
-                        borderRadius="$8"
-                        onPress={onPressNext}
-                        disabled={isDisabled()}
+        <ScrollView>
+            <YStack
+                px="$4"
+                justifyContent="space-between"
+            >
+                <YStack>
+                    <HeaderBackButton
+                        canGoBack={progress > ONBOARDING_PROGRESS_STEPS.GENDER_SELECT}
+                        onPress={onPressBack}
+                    />
+                    <Progress
+                        size="$1"
+                        value={progress}
+                        alignSelf="center"
+                        width="30%"
                     >
-                        {progress === ONBOARDING_PROGRESS_STEPS.BODY_INFO_SELECT ? 'Get my plan' : 'Next'}
-                    </Button>
+                        <Progress.Indicator
+                            backgroundColor="darkblue"
+                            animation="bouncy"
+                        />
+                    </Progress>
+                    {progress === ONBOARDING_PROGRESS_STEPS.GENDER_SELECT && (
+                        <OnboardingGenderSelect
+                            gender={gender}
+                            setGender={setGender}
+                        />
+                    )}
+                    {progress === ONBOARDING_PROGRESS_STEPS.FOCUS_AREA_SELECT && (
+                        <OnboardingFocusAreaSelect
+                            focusAreas={focusAreas}
+                            setFocusAreas={setFocusAreas}
+                        />
+                    )}
+                    {progress === ONBOARDING_PROGRESS_STEPS.DIFFICULTY_SELECT && (
+                        <OnboardingDifficultySelect
+                            difficulty={difficulty}
+                            setDifficulty={setDifficulty}
+                        />
+                    )}
+                    {progress === ONBOARDING_PROGRESS_STEPS.HEALTH_PROBLEM_SELECT && (
+                        <OnboardingHealthProbSelect
+                            healthProblems={healthProblems}
+                            setHealthProblems={setHealthProblems}
+                        />
+                    )}
+                    {progress === ONBOARDING_PROGRESS_STEPS.BODY_INFO_SELECT && (
+                        <OnboardingBodyInfoSelect
+                            height={height}
+                            weight={weight}
+                            setHeight={setHeight}
+                            setWeight={setWeight}
+                        />
+                    )}
                 </YStack>
-            </ScrollView>
-        </SafeAreaView>
+                <Button
+                    backgroundColor="darkblue"
+                    pressStyle={{ backgroundColor: '$blue11' }}
+                    disabledStyle={{ backgroundColor: '$blue7' }}
+                    color="white"
+                    m="$4"
+                    borderRadius="$8"
+                    onPress={onPressNext}
+                    disabled={isDisabled()}
+                >
+                    {progress === ONBOARDING_PROGRESS_STEPS.BODY_INFO_SELECT ? 'Get my plan' : 'Next'}
+                </Button>
+            </YStack>
+        </ScrollView>
     );
 }
