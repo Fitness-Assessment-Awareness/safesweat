58ac811da409318d47e018ae82b484f574e67aa2
weihanyau <weihanyau@gmail.com>
2024-09-24 23:15:12 +0800

    add space

--- app/context/UserProvider.tsx
+++ app/context/UserProvider.tsx
@@ -26,6 +26,7 @@ export function UserProvider({ children }: ComponentProps) {
 
 export function useUser() {
     const context = useContext(UserContext);
+
     if (!context) {
         throw new Error('useUser must be used within a UserProvider');
     }
