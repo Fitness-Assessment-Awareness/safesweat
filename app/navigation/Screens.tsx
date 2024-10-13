93c6c90a3845ae659f03af525460cdd0fc058e10
Wei Teng <weitengkoay@gmail.com>
2024-10-13 12:30:20 +0800

    fix: solve workoutProfile data delayed issue when determining initial route

--- app/navigation/Screens.tsx
+++ app/navigation/Screens.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/no-unstable-nested-components */
 import { BookMarked, Dumbbell, Globe2, Settings } from '@tamagui/lucide-icons';
+import { useEffect, useState } from 'react';
+import { Spinner, View } from 'tamagui';
 import { HeaderBackButton } from '../components/HeaderBackButton';
+import { Paragraph } from '../components/Paragraph';
 import { useWorkoutProfile } from '../context/WorkoutProfileProvider';
 import { CatalogueScreens } from '../modules/catalogue/navigation/CatalogueScreenGroup';
 import { ExploreScreens } from '../modules/explore/navigation/ExploreScreenGroup';
+import { Difficulty } from '../modules/onboarding/data/entities/Difficulty';
+import { Gender } from '../modules/onboarding/data/entities/Gender';
+import { OnboardingRootStackParamList } from '../modules/onboarding/navigation/OnboardingRootStackParamList';
 import { OnboardingScreenGroup } from '../modules/onboarding/navigation/OnboardingScreenGroup';
 import { SettingsScreens } from '../modules/settings/navigation/SettingsScreenGroup';
 import { WorkoutRootScreens } from '../modules/workout/navigation/WorkoutRootScreenGroup';
 import { WorkoutScreens } from '../modules/workout/navigation/WorkoutScreenGroup';
+import { WorkoutRootStackParamList } from '../modules/workout/navigation/WorkoutStackParamList';
 import { RootStack, Tab } from './AppNavigator';
 import { useRootNavigation } from './useAppNavigation';
 
@@ -78,6 +85,40 @@ function HomeTabScreens() {
 
 export function Screens() {
     const { workoutProfile } = useWorkoutProfile();
+    const [initialRouteName, setInitialRouteName] = useState<
+        'HomeTab' | keyof OnboardingRootStackParamList | keyof WorkoutRootStackParamList | undefined
+    >(undefined);
+    useEffect(() => {
+        const isOnboardingCompleted = () =>
+            workoutProfile &&
+            workoutProfile.difficulty !== Difficulty.NONE &&
+            workoutProfile.gender !== Gender.NONE &&
+            !Number.isNaN(workoutProfile.weight) &&
+            !Number.isNaN(workoutProfile.height) &&
+            workoutProfile.focusAreas.length > 0 &&
+            workoutProfile.healthProblems.length > 0;
+
+        const timer = setTimeout(() => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            isOnboardingCompleted() ? setInitialRouteName('HomeTab') : setInitialRouteName('OnboardingLanding');
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [workoutProfile]);
+
+    if (!initialRouteName) {
+        return (
+            <View
+                flex={1}
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Spinner size="large" />
+                <Paragraph m={8}>Setting up your profile...</Paragraph>
+            </View>
+        );
+    }
+
     return (
         <RootStack.Navigator
             id="root"
@@ -99,7 +140,7 @@ export function Screens() {
                 },
                 headerTintColor: 'black',
             }}
-            initialRouteName={workoutProfile ? 'HomeTab' : 'OnboardingLanding'}
+            initialRouteName={initialRouteName}
         >
             {OnboardingScreenGroup}
             <RootStack.Screen
