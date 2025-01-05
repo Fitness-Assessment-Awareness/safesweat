510824c0e82cbe25b797de0a15a17362497846b7
Wei Teng <weitengkoay@gmail.com>
2025-01-05 20:58:01 +0800

    enhancement: add new workout plans

--- app/modules/workout/assets/index.ts
+++ app/modules/workout/assets/index.ts
@@ -1,13 +1,19 @@
 import absBeginner from './abs-beginner.png';
-import chestBeginner from './chest-beginner.webp';
+import chest from './chest.png';
+import fullBody from './full-body.jpeg';
 import justRight from './just-right.png';
+import lowerBody from './lower-body.jpg';
 import tooEasy from './too-easy.png';
 import tooHard from './too-hard.png';
+import upperBody from './upper-body.jpg';
 
 export const WorkoutAssets = {
     absBeginner,
-    chestBeginner,
     tooEasy,
     tooHard,
     justRight,
+    upperBody,
+    lowerBody,
+    fullBody,
+    chest,
 } as const;
